9ec6183b057b9bbb7931af5dd68acc5519c91d53
Chu Manh <manhchth1805002@fpt.edu.vn>
2019-11-22 19:32:07 +0700

    Add project

---
+++ BaiKiemTra2/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BaiKiemTra2.Models
+{
+    public class Category
+    {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+        public string Picture { get; set; }
+
+    }
+}
---
+++ BaiKiemTra2/Models/MyDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace BaiKiemTra2.Models
+{
+
+    public class MyDbContext : DbContext
+    {
+        public System.Data.Entity.DbSet<BaiKiemTra2.Models.Category> Categories { get; set; }
+
+        public System.Data.Entity.DbSet<BaiKiemTra2.Models.Product> Products { get; set; }
+    }
+}
